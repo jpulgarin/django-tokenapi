ddb8a16ce0e4e6ae849cfcee1a0b5f974268ac4e
Julian Pulgarin <jp@julianpulgarin.com>
2011-02-24 05:11:28 -0500

    Tested with Django projecT

--- django_api/urls.py
+++ django_api/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('django_api.views',
     url(r'^token/new.json$', 'token_new', name="api_token_new"),
-    url(r'^token/(?P<token>.{24}).json$', 'token', name="api_token"),
+    url(r'^token/(?P<token>.{24})/(?P<user>\d+).json$', 'token', name="api_token"),
 )
--- django_api/views.py
+++ django_api/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseForbidden
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from django.views.decorators.csrf import csrf_exempt
 
 try: 
     import simplejson as json
@@ -25,30 +26,11 @@ def JSONError(error_string):
     return JSONResponse(data)
 
 
-# Checks if a given token and user pair is valid
-# token/:token.json
-# Required: user
-# Returns: success
-def token(request, token):
-    if 'user' in request.GET:
-        data = {}
-
-        try:
-            user = User.objects.get(pk=request.GET['user'])
-        except User.DoesNotExist:
-            return JSONError("User does not exist.")
-        if token_generator.check_token(user, 
-            token): 
-            data['success'] = True
-        else:
-            return JSONError("Token did not match user.")
-        return JSONResponse(data)
-
-
 # Creates a token if the correct username and password is given
-# token_new.json
+# token/new.json
 # Required: username&password
 # Returns: success&token&user&username
+@csrf_exempt
 def token_new(request):
     if request.method == 'POST':
         if 'username' in request.POST and 'password' in request.POST:
@@ -64,3 +46,20 @@ def token_new(request):
                 return JSONResponse(data)
             else:
                 return JSONError("Unable to log you in, please try again")
+
+# Checks if a given token and user pair is valid
+# token/:token/:user.json
+# Required: user
+# Returns: success
+def token(request, token, user):
+    data = {}
+    try:
+        user = User.objects.get(pk=user)
+    except User.DoesNotExist:
+        return JSONError("User does not exist.")
+    if token_generator.check_token(user, 
+        token): 
+        data['success'] = True
+    else:
+        return JSONError("Token did not match user.")
+    return JSONResponse(data)
