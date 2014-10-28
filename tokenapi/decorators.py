e464106d3bcf021710684e97b52d3ccb1c361765
Ian Zapolsky <ianzapolsky@gmail.com>
2014-10-28 03:00:55 -0400

    added decorator to allow decoration of functions which are members of class based views

--- tokenapi/decorators.py
+++ tokenapi/decorators.py
@@ -36,3 +36,37 @@ def token_required(view_func):
 
         return HttpResponseForbidden()
     return _wrapped_view
+
+def token_required_cb(view_func):
+    """Decorator specifically designed for a member function of a class based view,
+       which ensures the user has provided a correct user and token pair."""
+
+    @csrf_exempt
+    @wraps(view_func)
+    def _wrapped_view(class_instance, request, *args, **kwargs):
+        user = None
+        token = None
+        basic_auth = request.META.get('HTTP_AUTHORIZATION')
+
+        if basic_auth:
+            auth_method, auth_string = basic_auth.split(' ', 1)
+
+            if auth_method.lower() == 'basic':
+                auth_string = auth_string.strip().decode('base64')
+                user, token = auth_string.split(':', 1)
+
+        if not (user and token):
+            user = request.REQUEST.get('user')
+            token = request.REQUEST.get('token')
+
+            if not user or not token:
+                return HttpResponseForbidden("Must include 'user' and 'token' parameters with request.")
+
+        if user and token:
+            user = authenticate(pk=user, token=token)
+            if user:
+                login(request, user)
+                return view_func(class_instance, request, *args, **kwargs)
+
+        return HttpResponseForbidden()
+    return _wrapped_view
