58009a296ee6d6c7323bfcf4f31f5066c4fe18a4
Julian Pulgarin <jp@julianpulgarin.com>
2016-02-02 01:12:47 -0500

    Version 0.2.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='django-tokenapi',
-    version='0.2.3',
+    version='0.2.4',
     description='Add an API to your Django app using token-based authentication.',
     long_description=read('README.md'),
     author='Julian Pulgarin',
